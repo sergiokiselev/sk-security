d5b4c765e0c3a33a468375957853fc0bc092c2ef
seregakiselev <sammy_666@mail.ru>
2016-11-25 19:51:15 +0300

    File api updates

--- src/main/java/ch/rasc/sec/controller/FileController.java
+++ src/main/java/ch/rasc/sec/controller/FileController.java
@@ -1,16 +1,17 @@
 package ch.rasc.sec.controller;
 
+import ch.rasc.sec.dto.FileDescriptorDto;
 import ch.rasc.sec.dto.TokenDto;
 import ch.rasc.sec.dto.restresponse.ErrorDto;
 import ch.rasc.sec.dto.restresponse.RestResponse;
-import ch.rasc.sec.model.FileDescriptor;
 import ch.rasc.sec.service.GoogleApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 /**
@@ -23,17 +24,22 @@ public class FileController {
     @Autowired
     private GoogleApiService googleApiService;
 
+    @Value("${localMachine}")
+    private boolean isLocalMachine;
+
     @RequestMapping(method = RequestMethod.POST, value = "/files")
-    public RestResponse<FileDescriptor> uploadFile(@RequestBody MultipartFile file, @RequestParam String name,
-                                         @RequestParam String sessionId, @RequestParam long token) {
+    public RestResponse<FileDescriptorDto> uploadFile(@RequestBody MultipartFile file, @RequestParam String name,
+                                                      @RequestParam String sessionId, @RequestParam long token) {
         try {
-            java.io.File tempDir = new File("/tmp");
-            if (!tempDir.exists()) {
-                tempDir.mkdir();
+            String fileLocation = name;
+            if (!isLocalMachine) {
+                fileLocation = "/tmp/" + name;
             }
-            java.io.File file1 = new java.io.File(tempDir + "/" + name);
+            java.io.File file1 = new java.io.File(fileLocation);
             file1.createNewFile();
-            file.transferTo(file1);
+            FileOutputStream fileOutputStream = new FileOutputStream(file1);
+            fileOutputStream.write(file.getBytes());
+            fileOutputStream.close();
             return new RestResponse<>(googleApiService.uploadFile(file1, sessionId, token, name));
         } catch (Exception e) {
             e.printStackTrace();
@@ -42,9 +48,9 @@ public class FileController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/files")
-    public RestResponse<List<FileDescriptor>> getFilesList(@RequestBody TokenDto tokenDto) {
+    public RestResponse<List<FileDescriptorDto>> getFilesList(@RequestParam String sessionId, @RequestParam long token) {
         try {
-            return new RestResponse<>(googleApiService.getFilesList(tokenDto));
+            return new RestResponse<>(googleApiService.getFilesList(new TokenDto(sessionId, token)));
         } catch (Exception e) {
             e.printStackTrace();
             return new RestResponse<>(new ErrorDto(e.getMessage()));
@@ -52,18 +58,19 @@ public class FileController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/files/{fileId}")
-    public void deleteFile(@PathVariable String fileId, @RequestBody TokenDto tokenDto) {
+    public void deleteFile(@PathVariable String fileId, @RequestParam String sessionId, @RequestParam long token) {
         try {
-            googleApiService.deleteFile(fileId, tokenDto);
+            googleApiService.deleteFile(fileId, new TokenDto(sessionId, token));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/files/{fileId}")
-    public void downloadFile(@PathVariable String fileId, HttpServletResponse httpServletResponse) {
+    public void downloadFile(@PathVariable String fileId, @RequestParam String sessionId,
+                             @RequestParam long token, HttpServletResponse httpServletResponse) {
         try {
-            googleApiService.downloadFile(fileId, httpServletResponse.getOutputStream());
+            googleApiService.downloadFile(fileId, httpServletResponse, sessionId, token);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/ch/rasc/sec/service/impl/GoogleApiServiceImpl.java
+++ src/main/java/ch/rasc/sec/service/impl/GoogleApiServiceImpl.java
@@ -1,5 +1,6 @@
 package ch.rasc.sec.service.impl;
 
+import ch.rasc.sec.dto.FileDescriptorDto;
 import ch.rasc.sec.dto.TokenDto;
 import ch.rasc.sec.model.FileDescriptor;
 import ch.rasc.sec.model.SessionAttributes;
@@ -11,20 +12,21 @@ import ch.rasc.sec.service.GoogleApiService;
 import ch.rasc.sec.service.UserService;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.media.MediaHttpDownloader;
-import com.google.api.client.googleapis.media.MediaHttpUploader;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.ServletOutputStream;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -48,10 +50,10 @@ public class GoogleApiServiceImpl implements GoogleApiService {
     @Autowired
     private UserRepository userRepository;
 
-    private static final String UPLOAD_FILE_PATH = "src\\main\\resources\\download\\gopnik.jpeg";
-    private static final java.io.File UPLOAD_FILE = new java.io.File(UPLOAD_FILE_PATH);
+    @Value("${localMachine}")
+    private boolean isLocalMachine;
 
-    public FileDescriptor uploadFile(java.io.File file, String sessionId, long token, String name) throws Exception {
+    public FileDescriptorDto uploadFile(java.io.File file, String sessionId, long token, String name) throws Exception {
         userService.verifyToken(new TokenDto(sessionId, token));
         SessionAttributes sessionAttributes = userService.getSessionAttributes(sessionId);
         User user = userRepository.findOne(sessionAttributes.getUserId());
@@ -61,10 +63,8 @@ public class GoogleApiServiceImpl implements GoogleApiService {
         fileMetadata.setTitle(file.getName());
         FileContent mediaContent = new FileContent("image/jpeg", file);
         Drive.Files.Insert insert = drive.files().insert(fileMetadata, mediaContent);
-        MediaHttpUploader uploader = insert.getMediaHttpUploader();
-        uploader.setDirectUploadEnabled(true);
         File insertedFile = insert.execute();
-        return createFileDescriptor(name, user, insertedFile);
+        return mapToDto(createFileDescriptor(name, user, insertedFile));
     }
 
     private FileDescriptor createFileDescriptor(String name, User user, File insertedFile) {
@@ -79,28 +79,17 @@ public class GoogleApiServiceImpl implements GoogleApiService {
     }
 
     @Override
-    public List<FileDescriptor> getFilesList(TokenDto tokenDto) throws Exception {
+    public List<FileDescriptorDto> getFilesList(TokenDto tokenDto) throws Exception {
         userService.verifyToken(tokenDto);
         SessionAttributes sessionAttributes = userService.getSessionAttributes(tokenDto.getSessionId());
         User user = userRepository.getOne(sessionAttributes.getUserId());
         List<FileDescriptor> descriptors = fileDescriptorRepository.getByOwnerId(user);
-//        HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
-//        Drive drive = driveService.getDrive(transport);
-//
-//        Drive.Files.List list = drive.files()
-//        FileList fileList = list.execute();
-//
-//        List<String> ids = new ArrayList<>();
-//        for (File i : fileList.getItems()) {
-//            System.out.println(i);
-//            ids.add(i.getId());
-//        }
-//        return ids;
-        return descriptors;
+        return mapToDto(descriptors);
     }
 
     @Override
-    public void downloadFile(String fileId, ServletOutputStream outputStream) throws Exception {
+    public void downloadFile(String fileId, HttpServletResponse response, String sessionId, long token) throws Exception {
+        userService.verifyToken(new TokenDto(sessionId, token));
         HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
         Drive drive = driveService.getDrive(transport);
         Drive.Files.Get get = drive.files().get(fileId);
@@ -108,7 +97,20 @@ public class GoogleApiServiceImpl implements GoogleApiService {
         MediaHttpDownloader downloader =
                 new MediaHttpDownloader(transport, drive.getRequestFactory().getInitializer());
         downloader.setDirectDownloadEnabled(true);
-        downloader.download(new GenericUrl(file.getDownloadUrl()), outputStream);
+        String downloadPath = file.getTitle();
+        if (!isLocalMachine) {
+            downloadPath = "/tmp/" + downloadPath;
+        }
+        OutputStream out = new FileOutputStream(new java.io.File(downloadPath));
+        downloader.download(new GenericUrl(file.getDownloadUrl()), out);
+        out.close();
+        FileInputStream fis = new FileInputStream(downloadPath);
+        byte[] filebytes = new byte[fis.available()];
+        fis.read(filebytes);
+        OutputStream outputStream = response.getOutputStream();
+        response.setContentLength(filebytes.length);
+        outputStream.write(filebytes);
+        outputStream.close();
     }
 
     @Override
@@ -129,13 +131,24 @@ public class GoogleApiServiceImpl implements GoogleApiService {
         fileDescriptorRepository.delete(descriptor);
     }
 
-    /** Updates the name of the uploaded file to have a "drivetest-" prefix. */
-    private static File updateFileWithTestSuffix(String id, Drive drive) throws IOException {
-        File fileMetadata = new File();
-        fileMetadata.setTitle("drivetest-" + UPLOAD_FILE.getName());
+    private FileDescriptorDto mapToDto(FileDescriptor fileDescriptor) {
+        FileDescriptorDto fileDescriptorDto = new FileDescriptorDto();
+        fileDescriptorDto.setGoogleId(fileDescriptor.getGoogleId());
+        fileDescriptorDto.setLink(fileDescriptor.getLink());
+        fileDescriptorDto.setCreated(fileDescriptor.getCreated());
+        fileDescriptorDto.setName(fileDescriptor.getName());
+        fileDescriptorDto.setLastModified(fileDescriptor.getLastModified());
+        fileDescriptorDto.setOwnerId(fileDescriptor.getOwnerId().getId());
+        fileDescriptorDto.setSize(fileDescriptor.getSize());
+        return fileDescriptorDto;
+    }
 
-        Drive.Files.Update update = drive.files().update(id, fileMetadata);
-        return update.execute();
+    private List<FileDescriptorDto> mapToDto(List<FileDescriptor> descriptors) {
+        List<FileDescriptorDto> fileDescriptorDtos = new ArrayList<>();
+        for (FileDescriptor descriptor: descriptors) {
+            fileDescriptorDtos.add(mapToDto(descriptor));
+        }
+        return fileDescriptorDtos;
     }
 
 }
